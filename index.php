e038dde4989986056fd09f29fdf34661a510137d
DylanWilbrink <dylanwilbrink@gmail.com>
2018-02-06 15:26:00 +0100

    OP update

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
-<?php for ($x  =1; $x <= 5; $x++) {
+<?php for ($x  =1; $x <= 500; $x++) {
 	echo $x . '<iframe width="640" height="385" src="https://www.youtube.com/embed/GJDNkVDGM_s?autoplay=1"> </iframe> ';
 } ?>
  </body>
