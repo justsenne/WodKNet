f2844fdfd6b7d76f3c6c28ebede5b4b92f30ffba
jaimyvangerrevink <36194767+jaimyvangerrevink@users.noreply.github.com>
2018-02-06 15:04:55 +0100

    hihi

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  </head>
  <body>
 <?php for ($x  =1; $x <= 9999; $x++) {
-	echo $x . ' pornoacrtice <br>';
+	echo $x . ' <iframe width="560" height="315" src="https://www.youtube.com/embed/GJDNkVDGM_s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><br>';
 } ?>
  </body>
 </html>
