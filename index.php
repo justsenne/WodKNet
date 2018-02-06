a15400e123d91ca1273727559fc7e3dcc55f6b6a
jaimyvangerrevink <36194767+jaimyvangerrevink@users.noreply.github.com>
2018-02-06 15:05:53 +0100

    ja

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
-<?php for ($x  =1; $x <= 9999; $x++) {
+<?php for ($x  =1; $x <= 50; $x++) {
 	echo $x . ' <iframe width="560" height="315" src="https://www.youtube.com/embed/GJDNkVDGM_s" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><br>';
 } ?>
  </body>
