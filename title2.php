37e69e82441ec1ee712ef73cf731e67aa09f7802
jaimyvangerrevink <36194767+jaimyvangerrevink@users.noreply.github.com>
2018-03-06 12:18:51 +0100

    php

---
+++ title2.php
@@ -0,0 +1,5 @@
+<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image: url(images/heading-pages-05.jpg);">
+    <h2 class="l-text2 t-center">
+        Titel
+    </h2>
+</section>
