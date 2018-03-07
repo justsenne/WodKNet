9105f58f4bcde77649dc07670a0d07161ced8da4
justsenne <senne1320@gmail.com>
2018-03-07 08:42:30 +0100

    Begin login (login.php) en register.php

---
+++ login.php
@@ -0,0 +1,17 @@
+
+
+<html>
+<head>
+
+</head>
+<body>
+
+<form method="post" action="login.php">
+    Username: <input type="text" name="user"/>
+    Password: <input type="text" name="pass"/>
+    <input type="submit" name="submit" value="Login">
+
+</form>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Senne de Valk
+ * Date: 07/03/2018
+ * Time: 08:41
+ */
