cefe4924d4c72fa09799ed51a46c246167685047
justsenne <senne1320@gmail.com>
2018-04-03 13:03:47 +0200

    php nee

--- new/views/articleupdate.php
+++ new/views/articleupdate.php
@@ -1,10 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php
+    include("include.php");
+    ?>
+</head>
+
+<body>
+
 <?php
+include("header.php");
+
 include_once('connection.php');
-session_start();
 
 if (isset($_POST['submit'])) {
 
-    if ($_SESSION['rank'] == 1 || !isset($_SESSION["logged"])) {
+    if ($_SESSION['rank'] == 1 && isset($_SESSION["logged"])) {
 
         $sql = $conn->prepare("SELECT * FROM articles WHERE article_id=?");
         $sql->BindParam(1, $_POST['article']);
@@ -27,33 +39,35 @@ if (isset($_POST['submit'])) {
 
         header("Location: ../index.php");
         exit;
+    } else{
+        header("Location: ../index.php");
     }
+} else if ($_SESSION['rank'] == 1) {
+    $sql = $conn->prepare("SELECT * FROM articles WHERE article_id=?");
+    $sql->BindParam(1, $_POST['article']);
+    $sql->execute();
+    $result = $sql->fetchAll();
+
+    foreach ($result as $row) {
+        $name = $row['article_name'];
+        $desc = $row['article_description'];
+        $price = $row['article_price'];
+        $img = $row["article_image"];
+
+        echo "<form id='form' action='articleupdate.php?article=" . $_POST['article'] . "' method='POST' enctype='multipart/form-data'>
+           article name: <input type='text' maxlength='15' name='articlename' value='"
+            . $name
+            . "' required><br> article description (staat etc): <input type='text' maxlength='300' name='articledesc' value='"
+            . $desc
+            . "' required><br> Price: <input type='number' maxlength='6' name='articleprice' value='"
+            . $price
+            . "' required><br> <input type='submit' name='submit' value=" . $_POST['article'] . "> </form>";
+    }
+} else{
+    header("Location: ../index.php");
 }
 ?>
-
-<?php
-$sql = $conn->prepare("SELECT * FROM articles WHERE article_id=?");
-$sql->BindParam(1, $_POST['article']);
-$sql->execute();
-$result = $sql->fetchAll();
-
-foreach ($result as $row) {
-    $name = $row['article_name'];
-    $desc = $row['article_description'];
-    $price = $row['article_price'];
-    $img = $row["article_image"];
-
-    echo "<form id='form' action='articleupdate.php?article=" . $_POST['article'] . "' method='POST' enctype='multipart/form-data'>
-           article name: <input type='text' maxlength='15' name='articlename' value='"
-        . $name
-        . "' required><br> article description (staat etc): <input type='text' maxlength='300' name='articledesc' value='"
-           . $desc
-           ."' required><br> Price: <input type='number' maxlength='6' name='articleprice' value='"
-           . $price
-           ."' required><br> <input type='submit' name='submit' value=". $_POST['article'] . "> </form>";
-} ?>
-<button onclick="goBack()">Go Back</button>
-
+<button onclick="goBack()">Go Black</button>
 
 
 <a href="../index.php"> index </a>
@@ -61,7 +75,10 @@ foreach ($result as $row) {
 <script src="js\jquery.validate.js"></script>
 <script>
     $("#form").validate();
+
     function goBack() {
         window.history.back();
     }
 </script>
+</body>
+</html>
